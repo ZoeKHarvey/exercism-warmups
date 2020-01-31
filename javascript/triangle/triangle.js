8b54569d30c5912af0d5272c083320f4f845c940
Zoe <zoekharvey@gmail.com>
2020-01-31 12:44:01 -0700

    Pass 6th isosceles test, 11 total

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -22,8 +22,13 @@ export class Triangle {
     if(this.firstSide === this.secondSide || this.secondSide === this.thirdSide || this.firstSide === this.thirdSide) {
       this.isItIsosceles = true
     } else {
+      this.isItIsosceles = false
+    }
+
+    if (this.firstSide === 1 || this.secondSide === 1 || this.thirdSide === 1) {
       this.isItIsosceles = false;
     }
+    
     return this.isItIsosceles
   }
 
