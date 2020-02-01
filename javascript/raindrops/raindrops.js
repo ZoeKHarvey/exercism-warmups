a4940bf6007a8bd74b7b3151d9d4617a7a7b5e05
Zoe <zoekharvey@gmail.com>
2020-02-01 09:10:01 -0700

    Add factors of 7 to pass 4th raindrop test

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -10,8 +10,11 @@ export const convert = (num) => {
   if(num%3 === 0) {
     return 'Pling'
   }
-  if(num%5 ===0) {
+  if(num%5 === 0) {
     return 'Plang'
+  }
+  if(num%7 === 0) {
+    return 'Plong'
   }
 };
 
