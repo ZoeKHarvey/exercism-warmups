c4a0e950035aeef4610f6739778d8cd73128f390
Zoe <zoekharvey@gmail.com>
2020-02-10 18:09:23 -0700

    Pass first gigasecond test

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const gigasecond = (startDate) => {
+  const startTime = startDate.getTime();
+  const endDate = new Date(startTime + 1000000000000);
+  return endDate;
+}
